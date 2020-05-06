a0f08d07fb8270be138b5b90488da7d549bef407
vedanthv <vedanthvbaliga@gmail.com>
2020-05-06 15:12:31 +0530

    added functionWithArgs

--- JavaScript/fucntions.js
+++ JavaScript/fucntions.js
@@ -2,3 +2,11 @@ function reusableFunction() {
     console.log("Hi World");
 }
 reusableFunction();
+
+var sum;
+
+function functionWithArgs(a, b) {
+
+    console.log(a + b);
+}
+functionWithArgs(7, 10);
