f359b04d9163fb29ce7ecba0627fdebaf388d7b2
vedanthv <vedanthvbaliga@gmail.com>
2020-05-06 15:02:39 +0530

    added : shiftFunction

---
+++ JavaScript/shift()Function.js
@@ -0,0 +1,10 @@
+// That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
+// Setup
+var myArray = [
+    ["John", 23],
+    ["dog", 3]
+];
+
+// Only change code below this line
+var removedFromMyArray;
+removedFromMyArray = myArray.shift();
