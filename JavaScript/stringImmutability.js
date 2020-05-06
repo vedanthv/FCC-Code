6684c1ed2f729d00dac9406ab534334cf342db5b
vedanthv <vedanthvbaliga@gmail.com>
2020-05-06 14:41:40 +0530

    added: string immutibality

---
+++ JavaScript/stringImmutability.js
@@ -0,0 +1,18 @@
+// In JavaScript, String values are immutable, which means that they cannot be altered once created.
+//the individual characters cannot be changed
+// Setup
+// var myStr = "Jello World";
+
+// // Only change code below this line
+// myStr[0] = "H"; // Change this line
+// // Only change code above this line
+
+//the above does not work
+
+//this works
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
