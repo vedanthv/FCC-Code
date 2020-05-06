d6f66d137ef08df782bd4d5e8c9d0d992a98aceb
vedanthv <vedanthvbaliga@gmail.com>
2020-05-06 18:16:44 +0530

    added:dynamic prop creation

---
+++ JavaScript/dynamicPropCreation.js
@@ -0,0 +1,10 @@
+var someObj = {
+    propName: "John"
+};
+
+function propPrefix(str) {
+    var s = "prop";
+    return s + str;
+}
+var someProp = propPrefix("Name"); // someProp now holds the value 'propName'
+console.log(someObj[someProp]); // "John"
