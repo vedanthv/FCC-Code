3f1ac1725913a1806e1f3cbf30ea1a90d02f29bd
vedanthv <vedanthvbaliga@gmail.com>
2020-05-07 15:14:03 +0530

    modified:countDown.js

--- JavaScript/recursiveCountdown.js
+++ JavaScript/recursiveCountdown.js
@@ -1,14 +1,13 @@
-function countdown(n){
-    if(n <= 1)
-    return [];
-  else
-  {
-    const countArray = countdown(n-1);
-    countArray.unshift(n);
-    return countArray;
-  }
-  }
-  
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const arr = countdown(n - 1);
+        arr.unshift(n);
+        return arr;
+    }
+}
+
 /*As mentioned in the previous challenge, there will be a base
 case.The base
 case tells the recursive
